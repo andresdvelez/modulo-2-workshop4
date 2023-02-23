b1c64e5e1d64a8c999d88da39611b6cb1c1c8ddd
Andres David Velez Suaza <avelez0824@gmail.com>
2023-02-23 15:07:45 -0500

    create navbar

--- src/sections/HeroSection/HeroSection.jsx
+++ src/sections/HeroSection/HeroSection.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./style.scss";
+import BgImg from "../../assets/home/background-home-desktop.jpg";
 
 function HeroSection() {
-  return <div>HeroSection</div>;
+  return (
+    <div className="hero">
+      <img className="hero__bg" src={BgImg} alt="" />
+    </div>
+  );
 }
 
 export default HeroSection;
