dce9512b456c49cda88267747943173506f9c7c5
Danielaigm <danigarci852@gmail.com>
2023-02-23 18:30:22 -0500

    add slider

--- src/sections/CrewSection/CrewSection.jsx
+++ src/sections/CrewSection/CrewSection.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './style.scss'
+import React , { useEffect, useState } from "react";
+import Sliders from "../../components/Slider/Sliders";
+import { getData } from '../../services/getData'
 
-function CrewSection() {
-  return <div>CrewSection</div>;
+
+export default function CrewSection() {
+
+  const [listInfo, setListInfo] = useState([])
+  const endpoint = "crew"
+  const getCrew = async () => {
+    const response = await getData(endpoint);
+    setListInfo(response)
 }
 
-export default CrewSection;
+  useEffect(() => {
+    getCrew()
+}, [])
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return ( 
+    <main className="banner">
+      <p className="title"> <span className="number">02</span> MEET YOUR CREW</p>
+    <Slider {...settings}>
+      {
+                    listInfo.map((newSlider, index) => (
+                        <Sliders
+                            role={newSlider.role}
+                              bio={newSlider.bio}
+                              key={index}
+                              image={newSlider.images.png}
+                              name={newSlider.name}
+                        />
+                    ))
+                }
+    </Slider>
+    </main>
+  );
+}
