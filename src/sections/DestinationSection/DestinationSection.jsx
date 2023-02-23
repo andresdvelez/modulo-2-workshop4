07fc9c22d5aa5adecd1ced973d948fbf6234085e
ralexale <ralexale@gmail.com>
2023-02-23 17:43:14 -0500

    add destination page

--- src/sections/DestinationSection/DestinationSection.jsx
+++ src/sections/DestinationSection/DestinationSection.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import DestinationNavbar from '../../components/DestinationNavbar/DestinationNavbar';
+import './style.scss';
 
 function DestinationSection() {
-  return (
-    <div>DestinationSection</div>
-  )
+    return (
+        <>
+            <section className="destinatons">
+                <h2 className="title">
+                    <span className="number">01</span>Pick your destination
+                </h2>
+                <article className="destinationNavbar">
+                    <DestinationNavbar />
+                </article>
+            </section>
+        </>
+    );
 }
 
-export default DestinationSection
+export default DestinationSection;
